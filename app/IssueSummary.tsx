7c2cba7cc9f565ec2e04cbeaffb021bf4c214a52
Namgel <namgeltamang14@gmail.com>
2024-03-12 21:52:51 +0545

    Lay out the dashboard

--- app/IssueSummary.tsx
+++ app/IssueSummary.tsx
@@ -3,12 +3,14 @@ import { Card, Flex, Text } from "@radix-ui/themes";
 import Link from "next/link";
 
 interface Props {
-    open: number;
-    inProgress: number;
-    closed: number;
+    values: {
+        open: number;
+        inProgress: number;
+        closed: number;
+    };
 }
 
-const IssueSummary = ({ open, inProgress, closed }: Props) => {
+const IssueSummary = ({ values: { open, inProgress, closed } }: Props) => {
     const containers: {
         label: string;
         value: number;
